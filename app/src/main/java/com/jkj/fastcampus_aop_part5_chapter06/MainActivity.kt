c08409510764ef4614b7ccf9366d0e9123bc38a1
junggyungjin <junggyungjin@naver.com>
2023-08-28 22:40:23 +0900

    Initial commit

---
+++ app/src/main/java/com/jkj/fastcampus_aop_part5_chapter06/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jkj.fastcampus_aop_part5_chapter06
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
