221e7becbe4a29626dd2b1cadcf00c59aad50af7
junggyungjin <junggyungjin@naver.com>
2023-09-10 13:08:52 +0900

    project 환경 셋팅 에러 해결

--- app/src/main/java/com/jkj/fastcampus_aop_part5_chapter06/MainActivity.kt
+++ app/src/main/java/com/jkj/fastcampus_aop_part5_chapter06/MainActivity.kt
@@ -2,10 +2,23 @@ package com.jkj.fastcampus_aop_part5_chapter06
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jkj.fastcampus_aop_part5_chapter06.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initView()
+    }
+
+    private fun initView() {
+        val navigationController =
+            (supportFragmentManager.findFragmentById(R.id.mainNavigationHostContainer) as NavHostFragment).navController
+        binding.toolbar.setupWithNavController(navigationController)
     }
 }
--- app/src/main/java/com/jkj/fastcampus_aop_part5_chapter06/presentation/trackingitems/TrackingItemsFragment.kt
+++ app/src/main/java/com/jkj/fastcampus_aop_part5_chapter06/presentation/trackingitems/TrackingItemsFragment.kt
@@ -3,4 +3,6 @@ package com.jkj.fastcampus_aop_part5_chapter06.presentation.trackingitems
 import org.koin.android.scope.ScopeFragment
 
 class TrackingItemsFragment : ScopeFragment(), TrackingItemsContract.View{
+    override val presenter: TrackingItemsContract.Presenter
+        get() = TODO("Not yet implemented")
 }
